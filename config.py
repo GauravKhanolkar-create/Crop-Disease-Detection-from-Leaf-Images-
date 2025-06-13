6a9cbea4b577f17e1d57a43b4fd2d8fe5f59daaa
GauravKhanolkar-create <gaurav.g.khanolkar@gmail.com>
2025-06-13 12:30:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
-# config.py
-
 import streamlit as st
 
-# Streamlit page configuration
 st.set_page_config(page_title="Crop Disease Detection - Full Pipeline", layout="wide")
 
-# Global constants for image dimensions
 IMG_HEIGHT = 128
 IMG_WIDTH = 128
